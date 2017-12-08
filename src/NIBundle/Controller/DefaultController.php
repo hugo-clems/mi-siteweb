f0d9ceea91a82e9df46f29fb6e6778383dcce7f8
RookieRed <newgamer1396@gmail.com>
2017-12-08 03:44:12 +0100

    constructeur default controller

--- src/NIBundle/Controller/DefaultController.php
+++ src/NIBundle/Controller/DefaultController.php
@@ -3,11 +3,38 @@
 namespace NIBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class DefaultController extends Controller
 {
+
+    private $serializer;
+
+    public function __construct()
+    {
+        $encoders = [new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        $this->serializer = new Serializer($normalizers, $encoders);
+    }
+
     public function indexAction()
     {
         return $this->render('NIBundle:Default:index.html.twig');
+    }
+
+    public function coucouAction(){
+        $em = $this->getDoctrine()->getManager();
+
+        $users = $em->getRepository("NIBundle:Utilisateur")->findAll();
+
+        $rep = new Response($this->serializer->serialize($users, 'json'));
+
+        $rep->headers->set('Content-Type', 'application/json');
+        $rep->setCharset('utf-8');
+        return $rep;
     }
 }
